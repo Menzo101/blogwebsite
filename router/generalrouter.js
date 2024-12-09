fca7da68acc36cb645151e38f7aa23de8594a8be
oluwatobi <emmanuelshola045@gmail.com>
2024-12-09 11:35:13 -1100

    first commit

--- router/generalrouter.js
+++ router/generalrouter.js
@@ -3,10 +3,10 @@ const { protected, checkuser } = require("../middlewares/protected")
 const { getblog, postblog, deleteblog, getblogdetail } = require("../controller/generalcontroller");
 const router = express.Router()
 
-router.get('/blogs', checkuser, getblog)
+router.get('/', checkuser, getblog)
 // router.get('/blogs', postblog)
 router.post('/blogs', protected, postblog)
-router.get('/blogsdetails/:id?', checkuser,getblogdetail)
+router.get('/blogsdetails/:id?', checkuser, getblogdetail)
 router.get('/delete/:id?', protected, deleteblog)
 
 module.exports = router
